203219264115eaabff9b33f3c8bb27472bc0273c
Guilherme Huff <ghuff@alunos.utfpr.edu.br>
2022-10-15 23:35:07 -0300

    input e inicio home 94.

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,9 @@
 import Head from 'next/head';
+import Image from 'next/image';
+import styles from '../../styles/home.module.scss';
+
+import logoImg from '../../public/logo.svg';
+import { Input } from '../components/ui/Input';
 
 export default function Home() {
   return (
@@ -6,8 +11,20 @@ export default function Home() {
       <Head>
         <title>SujeitoPizza - Faça seu login</title>
       </Head>
-      <div>
-        <h1>SJP</h1>
+      <div className={styles.containerCenter}>
+        <Image src={logoImg} alt='Logo Sujeito Pizzaria' />
+        <div className={styles.login}>
+          <form>
+            <Input 
+              placeholder='Digite o seu email'
+              type='text'
+            />
+            <Input 
+              placeholder='Digite sua senha'
+              type='password'
+            />
+          </form>
+        </div>
       </div>
     </>
   )
