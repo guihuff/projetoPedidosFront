5d77cb4b918c0df98fa7c637a17d1fc9b1bf9ba6
guihuff <ghuff@alunos.utfpr.edu.br>
2023-01-16 19:51:05 -0300

    sistema de login

--- src/contexts/AuthContext.tsx
+++ src/contexts/AuthContext.tsx
@@ -1,10 +1,15 @@
+import { destroyCookie, parseCookies, setCookie } from 'nookies';
 import { createContext, ReactNode, useState } from 'react';
+import Router from 'next/router';
+import { api } from '../services/apiClient';
+import { setConfig } from 'next/config';
 
 
 type AuthContextData = {
   user: UserProps;
   isAuthenticated: boolean;
   singIn: (credentials: SingInProps) => Promise<void>;
+  singOut: () => void;
 }
 
 type UserProps = {
@@ -24,16 +29,51 @@ type AuthProviderProps = {
 
 export const AuthContext = createContext({} as AuthContextData);
 
+
+export function singOut() {
+  try {
+    destroyCookie(undefined, '@nextauth.token');
+    Router.push('/');
+  } catch (err) {
+    console.log('erro ao deslogar')
+  }
+}
+
 export function AuthProvider({ children }: AuthProviderProps){
   const [user, setUser] = useState<UserProps>();
   const isAuthenticated = !!user;
 
   async function singIn({ email, password } : SingInProps) {
-    console.log(email, password);
+    try{
+      const response = await api.post('/session', { email, password});
+      // console.log(response.data);
+
+      const { id, name, token } = response.data;
+      setCookie(undefined, '@nextauth.token', token, {
+        maxAge: 60 * 60 * 24 * 30,
+        path: "/"
+      });
+
+      setUser({
+        id,
+        name,
+        email
+      });
+
+      // Passar para proximas requisições o token
+
+      api.defaults.headers['Authorization'] = `Bearer ${token}`;
+
+      // Redirecionar para outra página
+
+      Router.push('/dashboard');
+    }catch(err){
+      console.log('Erro ao acessar', err);
+    }
   }
 
   return (
-    <AuthContext.Provider value={{ user, isAuthenticated, singIn }}>
+    <AuthContext.Provider value={{ user, isAuthenticated, singIn, singOut }}>
       {children}
     </AuthContext.Provider>
   )
