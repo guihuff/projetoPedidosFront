bb60bcfb37c666e29ad9daf7044f370b660412f0
guihuff <ghuff@alunos.utfpr.edu.br>
2023-01-19 10:34:14 -0300

    autenticação category

--- src/pages/category/index.tsx
+++ src/pages/category/index.tsx
@@ -3,6 +3,7 @@ import { FormEvent, useState } from "react";
 import { toast } from "react-toastify";
 import { Header } from "../../components/Header";
 import { setupAPIClient } from "../../services/api";
+import { canSSRAuth } from "../../utils/canSSRAuth";
 import styles from './styles.module.scss';
 
 export default function Category () {
@@ -54,3 +55,9 @@ export default function Category () {
     </>
   )
 }
+
+export const getServerSideProps = canSSRAuth(async (ctx) => {
+  return {
+    props: {}
+  }
+});
