af6f7cf5460ae905e442734432629565e6dd5516
guihuff <ghuff@alunos.utfpr.edu.br>
2023-01-19 15:16:30 -0300

    dashboard

--- src/pages/dashboard/index.tsx
+++ src/pages/dashboard/index.tsx
@@ -1,7 +1,9 @@
 import Head from "next/head";
-import { canSSRAuth } from "../../utils/canSSRAuth"
+import { canSSRAuth } from "../../utils/canSSRAuth";
+import styles from './styles.module.scss';
 
 import { Header } from '../../components/Header'
+import { FiRefreshCcw } from "react-icons/fi";
 
 export default function Dashboard () {
   return (
@@ -11,7 +13,23 @@ export default function Dashboard () {
     </Head>
     <div>
       <Header />
-      <h1>painelS</h1>
+      <main className={styles.container}>
+        <div className={styles.containerHeader}>
+          <h1>Últimos Pedidos</h1>
+          <button>
+            <FiRefreshCcw color="#3FFFa3" size={25}/>
+          </button>
+        </div>
+
+        <article className={styles.listOrders}>
+          <section className={styles.orderItem}>
+            <button>
+              <div className={styles.tag}></div>
+              <span>Mesa</span>
+            </button>
+          </section>
+        </article>
+      </main>
     </div>
     </>
   )
